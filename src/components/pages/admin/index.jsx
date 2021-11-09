a0b7840edc54f1d9b4281b9e372e844788794dc1
wangcong <2424772433@qq.com>
2021-11-09 23:11:11 +0800

    2021_11_09_03

--- src/components/pages/admin/index.jsx
+++ src/components/pages/admin/index.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import memoryUtils from '../../../utils/memoryUtils'
-// import {Redirect} from 'react-router-dom'
+import {Redirect} from 'react-router-dom'
 
 export default class Admin extends Component {
     render() {
-        // const user = memoryUtils.user
-        // if(!user||!user._id){
-        //     return <Redirect to='/login'/>
-        // }
+        const user = memoryUtils.user.username
+        if(!user||!user._id){
+            return <Redirect to='/login'/> //判断内存中有没有用户信息，如果没有就跳回登陆，禁止进入 后台
+        }
         return (
             <div>
-                Hellow:{memoryUtils.user.username}
+                Hellow:{user}
             </div>
         )
     }
